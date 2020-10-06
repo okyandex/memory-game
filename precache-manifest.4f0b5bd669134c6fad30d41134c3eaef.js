17aa1fba9b174345d78f83c39614f893a037bf6c
okyandex <avznn@yandex.ru>
2020-10-06 21:16:34 +0300

    Updates

---
+++ precache-manifest.4f0b5bd669134c6fad30d41134c3eaef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489f2c318550034b3047f59ef31b4727",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "7793d0c9beba34eadc3e",
+    "url": "/memory-game/static/css/main.a1a77e1f.chunk.css"
+  },
+  {
+    "revision": "8141a54dd48788f2bc91",
+    "url": "/memory-game/static/js/2.ef22af60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.ef22af60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7793d0c9beba34eadc3e",
+    "url": "/memory-game/static/js/main.372dd624.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
